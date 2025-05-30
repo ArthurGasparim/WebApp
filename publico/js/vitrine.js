6a71611c66d84d2af4cfde60f4e6bfb165317870
José Vitor <jvvmartos@hotmail.com>
2025-05-30 01:34:34 -0300

    Updated main (vitrine) page
    Better front-end and proper json-server data fetch integration

--- publico/js/vitrine.js
+++ publico/js/vitrine.js
@@ -1,21 +1,20 @@
-function carregarProdutos(){
-    fetch('https://fakestoreapi.com/products',{
+function loadProducts(){
+    fetch('http://localhost:4000/produtos',{
         method:"GET"
-    }).then((resposta) =>{
-        if (resposta.ok){
-            return resposta.json();
-        }
+    }).then((resposta) => {
+        if (resposta.ok) return resposta.json();
     }).then((listaDeProdutos) => {
         const divVitrine = document.getElementById("vitrine");
         for (const produto of listaDeProdutos){
             let card = document.createElement('div');
             card.innerHTML=`
-            <div class="card" style="width: 18rem;">
-                <img width="150px" height="250px" src="${produto.image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${produto.title}</h5>
-                    <p class="card-text">${produto.price}</p>
-                    <a href="#" class="btn btn-primary">Comprar</a>
+            <div class="card" style="width: 16rem; height: 25rem">
+                <img width="150px" height="200px" src="${produto.imagem}" class="card-img-top" alt="...">
+                <div class="card-body" style="display: flex; flex-direction: column;">
+                    <h5 class="card-title" style="font-size: 16px">${produto.nome}</h5>
+                    <p class="card-text">${produto.quantidade} unidades</p>
+                    <p class="card-text">$${produto.preco}</p>
+                    <a href="#" class="btn btn-primary" style="margin-top: auto;">Comprar</a>
                 </div>
             </div>
             `;
@@ -26,4 +25,4 @@ function carregarProdutos(){
     });
 }
 
-carregarProdutos();
+loadProducts();
